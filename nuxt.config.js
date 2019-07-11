520306b4bea5704fa0dc6ca4d9f3654a51715a20
Manu92 <eu_manu_92@yahoo.com>
2019-07-11 13:58:35 +0100

    dev environment variables for the Twitter API

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ** Environment variables
   */
  env: {
-  consumerKey:         process.env.NODE_ENV !== 'production' ? process.env.LOCAL_CONSUMER_KEY : process.env.CONSUMER_KEY,
-  consumerSecret:      process.env.NODE_ENV !== 'production' ? process.env.LOCAL_CONSUMER_SECRET : process.env.CONSUMER_SECRET,
-  accessToken:         process.env.NODE_ENV !== 'production' ? process.env.LOCAL_ACCESS_TOKEN : process.env.ACCESS_TOKEN,
-	accessTokenSecret:   process.env.NODE_ENV !== 'production' ? process.env.LOCAL_ACCESS_TOKEN_SECRET : process.env.ACCESS_TOKEN_SECRET
+  consumerKey:         process.env.NODE_ENV !== 'production' ? (process.env.LOCAL_CONSUMER_KEY ? process.env.LOCAL_CONSUMER_KEY : process.env.DEV_CONSUMER_KEY) : process.env.CONSUMER_KEY,
+  consumerSecret:      process.env.NODE_ENV !== 'production' ? (process.env.LOCAL_CONSUMER_SECRET ? process.env.LOCAL_CONSUMER_SECRET : process.env.DEV_CONSUMER_SECRET) : process.env.CONSUMER_SECRET,
+  accessToken:         process.env.NODE_ENV !== 'production' ? (process.env.LOCAL_ACCESS_TOKEN ? process.env.LOCAL_ACCESS_TOKEN : process.env.DEV_ACCESS_TOKEN) : process.env.ACCESS_TOKEN,
+	accessTokenSecret:   process.env.NODE_ENV !== 'production' ? (process.env.LOCAL_ACCESS_TOKEN_SECRET ? process.env.LOCAL_ACCESS_TOKEN_SECRET : process.env.DEV_ACCESS_TOKEN_SECRET) : process.env.ACCESS_TOKEN_SECRET
  },
 
   /*
