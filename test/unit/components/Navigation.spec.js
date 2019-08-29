cee5b039d840d163917aec2283001cfd06ed9f88
Natalie Smith <natalie.smith@moneysupermarket.com>
2019-08-29 09:56:23 +0100

    Added tests for Tweet card, DateTimeMixin and Navigation

---
+++ test/unit/components/Navigation.spec.js
@@ -0,0 +1,44 @@
+import Navigation from '@/components/Navigation.vue';
+import { mount } from 'vue-test-utils';
+
+describe('Navigation.vue', () => {
+	let cmp;
+
+	beforeEach(() => {
+		cmp = mount(Navigation);
+	});
+
+	it('should render with expected html structure', () => {
+		expect(cmp.element).toMatchSnapshot();
+	});
+
+	describe('desktop', () => {
+		it('nav is displayed ', () => {
+			expect(cmp.find('.nav__desktop').exists()).toBeTruthy();
+		});
+
+		it('emits scrollToSection event when item is clicked', () => {
+			cmp.find('.nav__item:first-child').trigger('click');
+			expect(cmp.emitted().scrollToSection).toBeTruthy();
+			expect(cmp.vm.isNavOpen).toBeFalsy();
+		});
+	});
+
+
+	describe('mobile', () => {
+		window.innerWidth = 700; // < 768px
+
+		it('mobile nav is displayed', () => {
+			expect(cmp.find('.nav__mobile').exists()).toBeTruthy();
+		});
+
+		it('nav items are displayed open', () => {
+			expect(cmp.find('.nav__mobile-burger').exists()).toBeTruthy();
+		});
+
+		it('nav items are closed', () => {
+			cmp.find('.nav__mobile').trigger('click');
+			expect(cmp.find('.nav__mobile-burger').classes()).toContain('nav__mobile-burger--open');
+		});
+	});
+});
---
+++ test/unit/components/TweetCard.spec.js
@@ -0,0 +1,46 @@
+import TweetCard from '@/components/TweetCard.vue';
+import { mount } from 'vue-test-utils';
+
+describe('TweetCard.vue', () => {
+	let cmp;
+
+	beforeEach(() => {
+		cmp = mount(TweetCard, {
+			propsData: {
+				tweets: [
+					{
+						user: {
+							screen_name: 'Gill2929',
+							name: 'Harry Hill',
+						},
+						text: 'twitter text here',
+						retweet_count: 5,
+						favorite_count: 15
+					}
+				]
+			}
+		});
+	});
+
+	it('should render with expected html structure', () => {
+		expect(cmp.element).toMatchSnapshot();
+	});
+
+	describe('twitter card', () => {
+		it('renders user name', () => {
+			expect(cmp.find('.tweet-card__profile-name').text()).toBe('Harry Hill');
+		});
+
+		it('renders tweet text', () => {
+			expect(cmp.find('.tweet-card__text').text()).toBe('twitter text here');
+		});
+
+		it('renders retweet count', () => {
+			expect(cmp.find('.tweet-card__count').text()).toBe('5');
+		});
+
+		it('renders likes count', () => {
+			expect(cmp.find('.tweet-card__likes').text()).toBe('15');
+		});
+	});
+});
