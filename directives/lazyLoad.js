a1862deed031867d11e9d4583e6ba0e61c15260d
Manu92 <eu_manu_92@yahoo.com>
2019-11-23 10:57:13 +0000

    Linting

--- directives/lazyLoad.js
+++ directives/lazyLoad.js
@@ -1,23 +1,23 @@
 /*
-	** Custom directive for lazy loading images.
+  ** Custom directive for lazy loading images.
 */
 
 export default {
   inserted: el => {
-		// Function responsible for replacing the src value with data-url.
+    // Function responsible for replacing the src value with data-url.
     function loadImage() {
       const imageElement = Array.from(el.children).find(el => el.nodeName === "IMG");
 			
       if (imageElement) {
-				// Listener that will fire a callback function when the loading is finished.
-				// Callback will be responsible for hiding the spinner and adding the animation (fade-in effect) to the image using a CSS class.
+        // Listener that will fire a callback function when the loading is finished.
+        // Callback will be responsible for hiding the spinner and adding the animation (fade-in effect) to the image using a CSS class.
         imageElement.addEventListener("load", () => setTimeout(() => el.classList.add("loaded"), 100));
         imageElement.addEventListener("error", () => console.log('error'));
         imageElement.src = imageElement.dataset.url;
       }
     }
 
-		// Intersection Observer’s function fired when it detects that the element enters the viewport or a parent component element.
+    // Intersection Observer’s function fired when it detects that the element enters the viewport or a parent component element.
     function handleIntersect(entries, observer) {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -27,8 +27,8 @@ export default {
       });
     }
 
-		// Responsible for creating our Intersection Observer and attaching it to our element.
-		// Accepts a callback that is fired when the observed element passes the specified threshold and the options object that carries our observer options.
+    // Responsible for creating our Intersection Observer and attaching it to our element.
+    // Accepts a callback that is fired when the observed element passes the specified threshold and the options object that carries our observer options.
     function createObserver() {
       const options = {
         root: null, // reference object used to base the visibility of our watched element
@@ -38,7 +38,7 @@ export default {
       observer.observe(el);
 		}
 		
-		// Falllback
+    // Falllback
     if (window["IntersectionObserver"]) {
       createObserver();
     } else {
